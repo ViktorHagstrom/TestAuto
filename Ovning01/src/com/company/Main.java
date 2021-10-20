a72e00a0dd6c9fe6a384af8662e9a903d4943ecb
Viktor Hagstrom <viktor.hagstrom@hotmail.se>
2021-10-20 12:02:22 +0200

    Ovning01 completed

--- Ovning01/src/com/company/Main.java
+++ Ovning01/src/com/company/Main.java
@@ -3,6 +3,16 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        int a = 20;
+        int b = 10;
+
+        System.out.println("A = " +a + "\nB = " + b);
+        
+        int tmp = a;
+        a=b;
+        b=tmp;
+
+        System.out.println("A = " +a + "\nB = " + b);
     }
 }
